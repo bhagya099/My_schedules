c2b56496489ea7a829b32845048718445b862ac4
Pooja Rao <raopooj@gmail.com>
2021-09-07 19:55:48 +1000

    Adding login route,login-form,styles.css

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
+const db = require('./database')
+const loginRouter = require('./routes/login')
 const app = express();
+
+// BODY PARSER
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+//Middleware
+app.use('/',loginRouter)
+
+// VIEW ENGINE
+app.set('view engine', 'ejs')
+
+//Set our static folder(CSS)
+ app.use(express.static('public'))
 
 const PORT = process.env.PORT || 3000;
 
