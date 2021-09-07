ca764cee390d8e998a2e0c375fd74b032236a8fa
Pooja Rao <raopooj@gmail.com>
2021-09-07 17:03:11 +1000

    Testing database on local system

--- sql/seed_table.sql
+++ sql/seed_table.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (firstname ,lastname ,email, password) 
 VALUES('bhagya', 'shree', 'bahbsd@aklsdf.com', 'asdf');
 
-INSERT INTO schedules(users_id, day, start_time, end_time) VALUES('4', 'MONDAY', '9:00AM', '11:00AM')
+INSERT INTO schedules(users_id, day, start_time, end_time) VALUES('2', 'MONDAY', '9:00AM', '11:00AM')
