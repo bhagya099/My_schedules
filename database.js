6ebf4d1a452d8e5b163b7f50f8b14e4fd8b6eea1
Bhagyashree <bhagya099@gmail.com>
2021-09-10 14:51:37 +1000

    create varable in databse file

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
-  
-// Proper way to initialize and share the Database object
+  // Proper way to initialize and share the Database object
 
-// Loading and initializing the library:
-const pgp = require('pg-promise')();
+  // Loading and initializing the library:
+  const pgp = require('pg-promise')();
 
-// Preparing the connection details:
-const cn = 'postgres://postgres:password@localhost:5432/my_schedules';
+  // Preparing the connection details:
+  const cn =
+      'postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:5432/my_schedules';
 
-// Creating a new database instance from the connection details:
-const db = pgp(cn);
+  // Creating a new database instance from the connection details:
+  const db = pgp(cn);
 
-// Exporting the database object for shared use:
-module.exports = db;
+  // Exporting the database object for shared use:
+  module.exports = db;
