fd443a369fd5319b7447e5aa15903223fb51d0e1
Bhagyashree <bhagya099@gmail.com>
2021-09-14 12:43:53 +1000

    create a homapage branch for displaying the schedules details

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
+const db = require('../database');
 router.get('/', (req, res) => {
-    res.render('pages/homepage');
+  //   res.render('pages/homepage');
+  db.any(
+    'SELECT users.users_id, users.firstname, users.lastname, users.email, schedules.start_time, schedules.end_time FROM users FULL OUTER JOIN schedules ON schedules.users_id = users.users_id ORDER BY users.users_id;'
+  )
+    .then((users) => {
+      console.log(users);
+      res.render('pages/homepage', {
+        users,
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
