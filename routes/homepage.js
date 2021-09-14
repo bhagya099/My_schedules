dd14f890ba322c8c6f3b7999f68bed41848bc5a1
Bhagyashree <bhagya099@gmail.com>
2021-09-14 13:22:00 +1000

    create a user route and link it to user name

--- routes/homepage.js
+++ routes/homepage.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     'SELECT users.users_id, users.firstname, schedules.start_time, schedules.end_time FROM users RIGHT JOIN schedules ON schedules.users_id = users.users_id ORDER BY users.users_id;'
   )
     .then((users) => {
-      console.log(users);
+      //  console.log(users);
       res.render('pages/homepage', {
         users,
       });
