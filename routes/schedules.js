52bb99e2954a0865bc4bd560dc3c06a9a110f916
Pooja Rao <raopooj@gmail.com>
2021-09-15 16:59:16 +1000

    adding schedule form to database

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = express.Router()
-//const { redirectToHome } = require('../middleware');
+const db = require('../database')
+const { redirectToLogin } = require('../middleware');
 
-router.get('/', (req,res) => {
+router.get('/', redirectToLogin, (req,res) => {
     res.render('pages/schedules')
 })
 
-// router.post('/', redirectToHome, (req, res) => {
-
-//})
+router.post('/', redirectToLogin, (req, res) => {
+     db.none('INSERT INTO schedules(users_id, day, start_time, end_time) VALUES($1, $2, $3, $4);', [req.body.users_id, req.body.day, req.body.start_time, req.body.end_time])
+  .then(() => {
+    res.redirect('/schedules')
+  })
+  .catch(error => {
+    console.log(error)
+    res.send(error)
+  })
+})
 module.exports = router
