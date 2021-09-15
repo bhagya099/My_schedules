f23c497f4a02026e5fdcbbfabd8ef869904279ba
Bhagyashree <bhagya099@gmail.com>
2021-09-15 17:00:30 +1000

    create a schedule get route and display the schedules only user log in

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,10 +1,26 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
+const db = require('../database');
 //const { redirectToHome } = require('../middleware');
 
-router.get('/', (req,res) => {
-    res.render('pages/schedules')
-})
+router.get('/', (req, res) => {
+  db.any('SELECT users_id, day, start_time, end_time FROM schedules;')
+    .then((schedules) => {
+      console.log(req.session.userId);
+      schedules.some((schedule) => {
+        if (schedule.users_id == req.session.userId) {
+          console.log(schedule);
+          res.render('pages/schedules', {
+            schedule,
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+});
 
 // router.post('/', redirectToHome, (req, res) => {
 
